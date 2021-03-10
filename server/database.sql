aabf2c98ed87b0a8371ad5e3b3b7a08ba5408550
eurowondollaryen <sehoakasayho@naver.com>
2021-03-10 18:27:36 +0900

    added time column

--- server/database.sql
+++ server/database.sql
@@ -3,5 +3,12 @@ CREATE DATABASE todopern;
 CREATE TABLE todo(
     todo_id SERIAL PRIMARY KEY,
     description VARCHAR(255),
-    done_yn VARCHAR(1) DEFAULT 'N'
+    done_yn VARCHAR(1) DEFAULT 'N',
+    inst_time VARCHAR(14),
+    done_time VARCHAR(14),
+    updt_time VARCHAR(14)
 );
+
+ALTER TABLE todo ADD COLUMN inst_time VARCHAR(14);
+ALTER TABLE todo ADD COLUMN done_time VARCHAR(14);
+ALTER TABLE todo ADD COLUMN updt_time VARCHAR(14);
